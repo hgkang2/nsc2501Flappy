cd77be56aecfdb19b3a0b07e1e26a501f74ad627
hgkang2 <hgkang2hank@naver.com>
2025-04-03 15:06:21 +0900

    11

---
+++ Assets/02.Scripts/MovePipe.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MovePipe : MonoBehaviour
+{
+    
+    [SerializeField] float speed = 0.65f;   
+    void Update()
+    {
+        //파이프의 위치를 speed만큼 좌로 이동
+        transform.position += Vector3.left * speed;
+    }
+}
